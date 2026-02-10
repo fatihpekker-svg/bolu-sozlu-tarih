6e51e0fb92d7b3583f0bc6e7c6f051b89b5f675f
Fatih Peker <fatihpeker@Fatih-MBP.hgw.local>
2026-02-10 19:22:34 +0300

    fix: force update studio page metadata

--- app/studio/[[...index]]/page.js
+++ app/studio/[[...index]]/page.js
@@ -3,8 +3,16 @@ import Studio from './Studio'
 // Required for static export
 export const dynamic = 'force-static'
 
-export { metadata } from 'next-sanity/studio/metadata'
-export { viewport } from 'next-sanity/studio/viewport'
+export const metadata = {
+    title: 'Sanity Studio',
+    robots: { index: false, follow: false }
+}
+
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1
+}
 
 export function generateStaticParams() {
     return [{ index: [] }]
